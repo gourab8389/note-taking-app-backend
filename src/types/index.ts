e81932858cdcec23d6ec4693cb54ceacfd5c0d48
gourab8389 <deyg6988@gmail.com>
2025-08-31 23:05:23 +0530

    chore: update AWS SDK dependencies to version 3.879.0 and related packages; modify TypeScript types and tsconfig for improved compatibility

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,5 @@
 import { Request } from 'express';
 
-// declare global {
-//   namespace Express {
-//     interface User {
-//       id: string;
-//       email: string;
-//       name: string;
-//     }
-//   }
-// }
-
 export interface AuthRequest extends Request {
   user?: {
     id: string;
